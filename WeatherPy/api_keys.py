233578081aae61bca9eed16c5c7ee8339cdd774d
agomeso <alejandrogomezmba@gmail.com>
2021-02-24 20:45:26 -0600

    set-up

--- WeatherPy/api_keys.py
+++ WeatherPy/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "YOUR KEY HERE!"
+weather_api_key = "d92b80712de1e3d00af15874cc5a0fb1"
 
 # Google API Key
-g_key = "YOUR KEY HERE!"
+g_key = "AIzaSyBsg0IgGyrCooL-nuvIRKpVJzFpVyzwFBo"
